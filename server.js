3f9747f97a0be1d9bed4e273401669127337d6ef
BhaskarMahale <bhaskarmahale271@gmail.com>
2026-02-25 19:27:52 +0530

    Removed node_modules and added gitignore

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Hey this my first project");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Load env 
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("HostelMate is Running ");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
